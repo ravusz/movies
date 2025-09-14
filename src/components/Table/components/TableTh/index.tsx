c2d78a06dcb1e0e0ed388d361e2f0e997697ab87
DESKTOP-LGF9G09\rav <ravusz91@gmail.com>
2025-09-14 17:38:31 +0200

    update table header

--- src/components/Table/components/TableTh/index.tsx
+++ src/components/Table/components/TableTh/index.tsx
@@ -4,6 +4,8 @@ type TableThProps = React.ComponentProps<"th"> & {
 
 export async function TableTh({ children, className = "", ...restProps }: TableThProps) {
   return (
-    <th className={`px-3 py-2 whitespace-nowrap ${className}`} {...restProps}>{children}</th>
+    <th className={`px-3 py-2 whitespace-nowrap ${className}`} {...restProps}>
+      <span className="font-semibold">{children}</span>
+    </th>
   );
 }
--- src/styles/fonts/index.tsx
+++ src/styles/fonts/index.tsx
@@ -1,3 +1,7 @@
 import { Montserrat } from "next/font/google";
 
-export const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-montserrat" });
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  display: "swap",
+});
